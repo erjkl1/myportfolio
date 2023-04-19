093b117b8a5696ccd4a22629cad23db390e4d0b5
chuke <chuke@Chuke.local>
2023-04-20 07:00:50 +0900

    About修正中

--- src/About.tsx
+++ src/About.tsx
@@ -1,63 +1,57 @@
-import React from 'react';
-import Chuke from './img/chuke.png';
-import TemporaryDrawer from './TemporaryDrawer';
-import Timeline from './Timeline'
-import TwitterIcon from '@mui/icons-material/Twitter';
-import ArticleIcon from '@mui/icons-material/Article';
+import React from "react";
+import Chuke from "./img/chuke.png";
+import TemporaryDrawer from "./TemporaryDrawer";
+import Timeline from "./Timeline";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import ArticleIcon from "@mui/icons-material/Article";
 
 const About = () => {
   const drawerButtonStyle = {
-    position:'absolute',
-    left: '0',
-    top: '0',
-    width:'110px',
-    height:'100%',
-    color:'#ffffff',
+    position: "absolute",
+    left: "0",
+    top: "0",
+    width: "110px",
+    height: "100%",
+    color: "#ffffff",
     "&:hover": {
-      backgroundColor:'#171717',
-      cursor: 'default',
-    }
+      backgroundColor: "#171717",
+      cursor: "default",
+    },
   };
-  
-  return (
-    // <div style={{ backgroundImage: `url(${Camp})`}} className='fixed top-0 left-0 h-screen w-full'> 
-    <div className='h-screen w-screen'> 
-      <TemporaryDrawer 
-      sx={drawerButtonStyle}
-      
-      />
-      <div className='flex justify-center py-4 text-6xl'>
-        About
-      </div>
-      
-      <div className = 'flex justify-center'>
 
-        <div className='w-2/5 mr-20'>
-          <div className='flex justify-center'>
-              <img src={Chuke} alt="Chuke" className='w-40'/>
+  return (
+    // <div style={{ backgroundImage: `url(${Camp})`}} className='fixed top-0 left-0 h-screen w-full'>
+    <div className="h-screen w-screen">
+      <TemporaryDrawer sx={drawerButtonStyle} />
+      <div className="flex justify-center py-4 text-6xl">About</div>
+      <div className="flex justify-center">
+        <div className="w-2/5 mr-20">
+          <div className="flex justify-center">
+            <img src={Chuke} alt="Chuke" className="w-40" />
           </div>
-          <div className='flex justify-center text-xl my-2'>
-              ちゅけ
-          </div>
-          <div className='flex justify-center'>
+          <div className="flex justify-center text-xl my-2">ちゅけ</div>
+          <div className="flex justify-center">
             <a href="https://twitter.com/Chuke_yamaha">
-              <TwitterIcon sx={{fontSize:50}}/>
+              <TwitterIcon sx={{ fontSize: 30 }} />
             </a>
             <a href="https://chuke-blog.com/Chuke_yamaha">
-              <ArticleIcon sx={{fontSize:50}}/>
+              <ArticleIcon sx={{ fontSize: 30 }} />
             </a>
           </div>
-
-          
         </div>
 
-        <div className = 'w-2/5'>
-          <div className = 'text-xl'>
-            埼玉県生まれ。法政大学　機械工学科卒業。<br/>
-            大手自動車メーカーにて完成者の品質保証に携わる。<br/>
-            在職中pythonを用いてデータからは読み取れないMT車のギアを推測、<br/>
-            車速ごとの使用頻度分布を算出するソフトを開発。<br/>
-            2022年より研修会社に社内SEとして転職。社内向けWEBアプリ開発を主業務としている。<br/>
+        <div className="w-2/5">
+          <div className="text-lg">
+            埼玉県生まれ。法政大学　機械工学科卒業。
+            <br />
+            大手自動車メーカーにて完成者の品質保証に携わる。
+            <br />
+            在職中pythonを用いてデータからは読み取れないMT車のギアを推測、
+            <br />
+            車速ごとの使用頻度分布を算出するソフトを開発。
+            <br />
+            2022年より研修会社に社内SEとして転職。社内向けWEBアプリ開発を主業務としている。
+            <br />
           </div>
           <br />
           {/* <div className = ''>
@@ -68,16 +62,12 @@ const About = () => {
             </div> */}
         </div>
       </div>
-      <div className='flex justify-center w-full text-5xl'>
-        経歴
-      </div>
-      <div className='flex justify-center w-11/12 h-3/5'>
+      <div className="flex justify-center w-full text-xl">経歴</div>
+      <div className="flex justify-center w-11/12 h-3/5">
         <Timeline />
       </div>
     </div>
   );
-}
-
-
+};
 
 export default About;
