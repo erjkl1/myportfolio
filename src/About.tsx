aa28cb6f3f9a7b281018d82d5fc87ebe63ea52c4
chuke <chuke@Chuke.local>
2023-04-21 07:05:53 +0900

    メニュー改修 Aboutメニュー押せない問題解消

--- src/About.tsx
+++ src/About.tsx
@@ -23,46 +23,48 @@ const About = () => {
     // <div style={{ backgroundImage: `url(${Camp})`}} className='fixed top-0 left-0 h-screen w-full'>
     <div className="h-screen w-screen">
       <TemporaryDrawer sx={drawerButtonStyle} />
-      <div className="flex justify-center py-4 text-6xl">About</div>
-      <div className="flex justify-center">
-        <div className="w-2/5 mr-20">
-          <div className="flex justify-center">
-            <img src={Chuke} alt="Chuke" className="w-40" />
+      <div className="h-2/5">
+        <div className="flex justify-center py-4 text-6xl">About</div>
+        <div className="flex justify-center">
+          <div className="w-2/5 mr-20">
+            <div className="flex justify-center">
+              <img src={Chuke} alt="Chuke" className="w-36" />
+            </div>
+            <div className="flex justify-center text-xl my-2">ちゅけ</div>
+            <div className="flex justify-center">
+              <a href="https://twitter.com/Chuke_yamaha">
+                <TwitterIcon sx={{ fontSize: 30 }} />
+              </a>
+              <a href="https://chuke-blog.com/Chuke_yamaha">
+                <ArticleIcon sx={{ fontSize: 30 }} />
+              </a>
+            </div>
           </div>
-          <div className="flex justify-center text-xl my-2">ちゅけ</div>
-          <div className="flex justify-center">
-            <a href="https://twitter.com/Chuke_yamaha">
-              <TwitterIcon sx={{ fontSize: 30 }} />
-            </a>
-            <a href="https://chuke-blog.com/Chuke_yamaha">
-              <ArticleIcon sx={{ fontSize: 30 }} />
-            </a>
-          </div>
-        </div>
 
-        <div className="w-2/5">
-          <div className="text-lg">
-            埼玉県生まれ。法政大学　機械工学科卒業。
+          <div className="w-2/5">
+            <div className="text-lg">
+              埼玉県生まれ。法政大学　機械工学科卒業。
+              <br />
+              大手自動車メーカーにて完成者の品質保証に携わる。
+              <br />
+              在職中pythonを用いてデータからは読み取れないMT車のギアを推測、
+              <br />
+              車速ごとの使用頻度分布を算出するソフトを開発。
+              <br />
+              2022年より研修会社に社内SEとして転職。社内向けWEBアプリ開発を主業務としている。
+              <br />
+            </div>
             <br />
-            大手自動車メーカーにて完成者の品質保証に携わる。
-            <br />
-            在職中pythonを用いてデータからは読み取れないMT車のギアを推測、
-            <br />
-            車速ごとの使用頻度分布を算出するソフトを開発。
-            <br />
-            2022年より研修会社に社内SEとして転職。社内向けWEBアプリ開発を主業務としている。
-            <br />
-          </div>
-          <br />
-          {/* <div className = ''>
+            {/* <div className = ''>
             クリフトンストレングス<br />
-          </div>
+            </div>
             <div className=''>
-              <img src={Clifton} alt="Crifton" className='w-80'/>
-            </div> */}
+            <img src={Clifton} alt="Crifton" className='w-80'/>
+          </div> */}
+          </div>
         </div>
+        <div className="flex justify-center w-full text-xl">経歴</div>
       </div>
-      <div className="flex justify-center w-full text-xl">経歴</div>
       <div className="flex justify-center w-11/12 h-3/5">
         <Timeline />
       </div>
