8f2547c6ef5823ccddd2c4bb34d28a6ca89837ac
chuke <chuke@Chuke.local>
2023-04-22 08:04:13 +0900

    Add homepage

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,37 +1,36 @@
-import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import History from './History';
-import Skill from './Skill/Skill';
-import Work from './Work/Work';
-import Contact from './Contact/Contact';
-import NoMatch from './NoMatch';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import History from "./History";
+import Skill from "./Skill/Skill";
+import Work from "./Work/Work";
+import Contact from "./Contact/Contact";
+import NoMatch from "./NoMatch";
 
-const Routing = () =>{
-    type routeConfig ={
-      [address : string]: JSX.Element;
-    };
-    const routing:routeConfig = {
-       home:<Home />, 
-       about: <About />, 
-      //  history: <History />, 
-       skill: <Skill />,
-       contact: <Contact />,
-       work: <Work />,
-      }
-    const componentsKey=Object.keys(routing);
-    return(
-      <BrowserRouter>
-        <Routes>
-                <Route path="/" element = {<Home />} />
-                <Route path="*" element={<NoMatch />} />
-                {componentsKey.map((key) =>
-                (
-                <Route path={'/'+key} element = {routing[key]} key={key} />
-                ))}
-        </Routes>
-      </BrowserRouter>
-    )
-}
+const Routing = () => {
+  type routeConfig = {
+    [address: string]: JSX.Element;
+  };
+  const routing: routeConfig = {
+    home: <Home />,
+    about: <About />,
+    //  history: <History />,
+    skill: <Skill />,
+    contact: <Contact />,
+    work: <Work />,
+  };
+  const componentsKey = Object.keys(routing);
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NoMatch />} />
+        {componentsKey.map((key) => (
+          <Route path={"/" + key} element={routing[key]} key={key} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default Routing;
