521adae7fc5bcd306e1d957bd3b9c7807bd1ba39
chuke <wanwano.game@gmail.com>
2024-03-08 17:04:56 +0900

    fix About and Skill

--- src/About.tsx
+++ src/About.tsx
@@ -6,69 +6,65 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import ArticleIcon from "@mui/icons-material/Article";
 
 const About = () => {
-  const drawerButtonStyle = {
-    position: "absolute",
-    left: "0",
-    top: "0",
-    width: "110px",
-    height: "100%",
-    color: "#ffffff",
-    "&:hover": {
-      backgroundColor: "#171717",
-      cursor: "default",
-    },
-  };
+	const drawerButtonStyle = {
+		position: "absolute",
+		left: "0",
+		top: "0",
+		width: "110px",
+		height: "100%",
+		color: "#ffffff",
+		"&:hover": {
+			backgroundColor: "#171717",
+			cursor: "default",
+		},
+	};
 
   return (
-    // <div style={{ backgroundImage: `url(${Camp})`}} className='fixed top-0 left-0 h-screen w-full'>
-    <div className="h-screen w-screen">
-      <TemporaryDrawer sx={drawerButtonStyle} />
-      <div className="h-2/5">
-        <div className="flex justify-center py-4 text-6xl">About</div>
-        <div className="flex justify-center">
-          <div className="w-2/5 mr-20">
-            <div className="flex justify-center">
-              <img src={Chuke} alt="Chuke" className="w-36" />
-            </div>
-            <div className="flex justify-center text-xl my-2">ちゅけ</div>
-            <div className="flex justify-center">
-              <a href="https://twitter.com/Chuke_yamaha">
-                <TwitterIcon sx={{ fontSize: 30 }} />
-              </a>
-              <a href="https://chuke-blog.com/Chuke_yamaha">
-                <ArticleIcon sx={{ fontSize: 30 }} />
-              </a>
-            </div>
-          </div>
-
-          <div className="w-2/5">
-            <div className="text-lg">
-              埼玉県生まれ。法政大学　機械工学科卒業。
-              <br />
-              大手自動車メーカーにて完成者の品質保証に携わる。
-              <br />
-              在職中pythonを用いてデータからは読み取れないMT車のギアを推測、
-              <br />
-              車速ごとの使用頻度分布を算出するソフトを開発。
-              <br />
-              2022年より研修会社に社内SEとして転職。社内向けWEBアプリ開発を主業務としている。
-              <br />
-            </div>
-            <br />
-            {/* <div className = ''>
-            クリフトンストレングス<br />
-            </div>
-            <div className=''>
-            <img src={Clifton} alt="Crifton" className='w-80'/>
-          </div> */}
-          </div>
-        </div>
-        <div className="flex justify-center w-full text-xl">経歴</div>
-      </div>
-      <div className="flex justify-center w-11/12 h-3/5">
-        <Timeline />
-      </div>
-    </div>
+	  // <div style={{ backgroundImage: `url(${Camp})`}} className='fixed top-0 left-0 h-screen w-full'>
+	  <div className="h-screen">
+		  <TemporaryDrawer sx={drawerButtonStyle} />
+		  <div>
+			  <div className="flex justify-center py-4 text-6xl">About</div>
+			  <div className="flex justify-center">
+				  <div className="w-2/5 mr-20">
+					  <div className="flex justify-center">
+						  <img src={Chuke} alt="Chuke" className="w-36" />
+					  </div>
+					  <div className="flex justify-center text-xl my-2">ちゅけ</div>
+					  <div className="flex justify-center">
+						  <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/Chuke_yamaha">
+							  <TwitterIcon sx={{ fontSize: 30 }} />
+						  </a>
+						  <a target="_blank" rel="noopener noreferrer" href="https://chuke-blog.com/" >
+							  <ArticleIcon sx={{ fontSize: 30 }} />
+						  </a>
+					  </div>
+				  </div>
+				  <div className="w-2/5">
+					  <div className="text-lg">
+						  埼玉県川越市生まれ。法政大学　機械工学科卒業。<br />
+						  大手自動車メーカーにて完成者の品質保証に携わる。<br />
+						  在職中pythonを用いてデータからは読み取れないMT車のギアを推測、<br />
+						  車速ごとの使用頻度分布を算出するソフトを開発。<br />
+						  2022年より研修会社に社内SEとして転職。<br />
+						  社内向けWEBアプリ開発を主業務としている。<br />
+					  </div>
+					  <br />
+					  {/* 
+					  	<div className = ''>
+							クリフトンストレングス<br/>
+						</div>
+						<div className=''>
+							<img src={Clifton} alt="Crifton" className='w-80'/>
+						</div> */}
+				  </div>
+			  </div>
+			  <div className="flex justify-center w-full text-xl">経歴</div>
+		  </div>
+		  <div className="flex justify-center w-11/12">
+			  <Timeline />
+		  </div>
+	  </div>
   );
 };
 
--- src/Skill/SkillCard.tsx
+++ src/Skill/SkillCard.tsx
@@ -1,29 +1,43 @@
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import CardActions from '@mui/material/CardActions';
 import Typography from "@mui/material/Typography";
+import { Button } from '@mui/material';
 
 type SkillCard = {
-  title: string;
-  body: string;
-  icon: string;
+	title: string;
+	body: string;
+	icon: string;
+	link: string;
 };
 
-export default function SkillCard({ title, body, icon }: SkillCard) {
+export default function SkillCard({ title, body, icon, link }: SkillCard) {
   return (
-    <Card sx={{ minWidth: 200, backgroundColor: "white", color: "black" }}>
-      <CardContent sx={{ borderColor: "black" }}>
-        <Typography variant="h5" component="div">
-          <div className="flex justify-center">
-            <img src={icon} className="w-20" />
-          </div>
-          {title}
-        </Typography>
-        <hr className="border-gray-500 my-1" />
-        <Typography variant="body2">{body}</Typography>
-      </CardContent>
-      {/* <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions> */}
-    </Card>
+	  <Card sx={{ minWidth: 200, backgroundColor: "white", color: "black" }}>
+		  <CardContent sx={{ borderColor: "black" }}>
+			  <Typography variant="h5" component="div">
+				  <div className="flex justify-center">
+					  <img src={icon} className="w-20" />
+				  </div>
+				  {title}
+			  </Typography>
+			  <hr className="border-gray-500 my-1" />
+			  <Typography variant="body2">{body}</Typography>
+		  </CardContent>
+
+		  {link != undefined && (
+			  <CardActions >
+				  <Button
+					  component="a" // ここでaタグを指定
+					  href={`${link}`}
+					  target="_blank"
+					  rel="noopener noreferrer"
+					  color="primary"
+				  >
+					  外部リンク
+				  </Button>
+			  </CardActions>
+		  )}
+	  </Card>
   );
 }
